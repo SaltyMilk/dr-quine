048190e0129d9875c3a057563d7b8cefdccb92a0
SaltyMilk <soufiane.elmelcaoui@gmail.com>
2020-12-09 15:04:13 +0100

    Initial commit

---
+++ ASM/Colleen.s
@@ -0,0 +1,10 @@
+section .text
+global _main
+fun:
+ret
+_main:
+call fun
+ret
+section .data
+format db ''
+
---
+++ C/Colleen.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ /*
+ This program will print its own source when run.
+ */
+void fun(){return;}
+int main()
+{
+ /*
+ This is the source code
+ */
+fun();
+char str[]="#include<stdio.h>%c /*%1$c This program will print its own source when run.%1$c */%1$cvoid fun(){return;}%1$cint main()%1$c{%1$c /*%1$c This is the source code%1$c */%1$cfun();%1$cchar str[]=%c%s%2$c;%1$cprintf(str, 10, 34, str);%1$c}";
+printf(str, 10, 34, str);
+}
